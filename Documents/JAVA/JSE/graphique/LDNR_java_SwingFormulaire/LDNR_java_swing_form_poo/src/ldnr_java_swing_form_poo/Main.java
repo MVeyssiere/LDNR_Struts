80e8b485da023dec40373add94583ea2f0f5fc1b
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-17 14:04:01 +0100

    Main file.

---
+++ Documents/JAVA/JSE/graphique/LDNR_java_SwingFormulaire/LDNR_java_swing_form_poo/src/ldnr_java_swing_form_poo/Main.java
@@ -0,0 +1,15 @@
+package ldnr_java_swing_form_poo;
+
+/**
+ *
+ * @author Marine V
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulaire formulaire = new Formulaire();
+    }
+}
